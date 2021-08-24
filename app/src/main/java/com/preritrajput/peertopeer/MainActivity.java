119f447df528d382578702097f6240c1a39d7f1e
Shubham <sunidhi21thakur@gmail.com>
2021-08-24 21:16:20 +0530

    Splash and sign-up screens

--- app/src/main/java/com/preritrajput/peertopeer/MainActivity.java
+++ app/src/main/java/com/preritrajput/peertopeer/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Hello
+        setContentView(R.layout.activity_splash_login);
+
+        LottieAnimationView animationView;
+        animationView =  findViewById(R.id.imgView_logo);
+
+        animationView.playAnimation();
+
     }
 }
