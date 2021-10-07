3a6ec9fc1fd2477f8087c9d51c59d7e32392d9c0
Gunjan Saini <gunjansaini456123@gmail.com>
2021-10-07 13:46:47 +0530

    Screen Added

--- app/src/main/java/com/preritrajput/peertopeer/fragments/HomeFragment.java
+++ app/src/main/java/com/preritrajput/peertopeer/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.preritrajput.peertopeer.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.preritrajput.peertopeer.ListCarActivity;
 import com.preritrajput.peertopeer.R;
+import com.preritrajput.peertopeer.databinding.FragmentHomeBinding;
 
 
 public class HomeFragment extends Fragment {
 
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -21,7 +25,16 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        binding=FragmentHomeBinding.inflate(inflater, container, false);
+        View view=binding.getRoot();
+
+        binding.gotToListCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ListCarActivity.class));
+            }
+        });
+
         return view;
     }
 }
