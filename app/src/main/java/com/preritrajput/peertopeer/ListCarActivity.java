58f2c35c937fdc6ee1251a7da658ce869802c92e
Gunjan Saini <gunjansaini456123@gmail.com>
2021-10-11 17:30:30 +0530

    Fixed some error

--- app/src/main/java/com/preritrajput/peertopeer/ListCarActivity.java
+++ app/src/main/java/com/preritrajput/peertopeer/ListCarActivity.java
@@ -1,6 +1,7 @@
 package com.preritrajput.peertopeer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,7 +16,12 @@ public class ListCarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_car);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.black_trans80));
+        binding = ActivityListCarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         binding.saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
